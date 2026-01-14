ae506b4bcacce7870cdf67c879560db781a6852a
Sivasankar <sivasankar6305@gmail.com>
2026-01-14 18:16:16 +0530

    Completed Quantium Task 2 - Data Processing

---
+++ data/process_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import glob
+
+csv_files = glob.glob("data/*.csv")
+
+df_list = []
+for file in csv_files:
+    df = pd.read_csv(file)
+    df_list.append(df)
+
+
+combined_df = pd.concat(df_list, ignore_index=True)
+
+pink_df = combined_df[combined_df["product"] == "Pink Morsel"]
+
+pink_df["sales"] = pink_df["quantity"] * pink_df["price"]
+
+
+final_df = pink_df[["sales", "date", "region"]]
+
+
+final_df.to_csv("output.csv", index=False)
+
+print("✅ Data processing completed")
